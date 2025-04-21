98eaeee96f0f161b51fb59c2fb018efd4dd1d436
Olivierkepler <olivierkfrancois1@gmail.com>
2025-04-21 13:23:00 -0400

    update with them

--- components/carousselSection/hero.tsx
+++ components/carousselSection/hero.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { IconCheck } from '@tabler/icons-react';
+// import { IconCheck } from '@tabler/icons-react';
 // import SocialLinksVertical from '@/components/carousselSection/SocialLinks';  
 import HeroInfo from '@/components/carousselSection/heroInfo';
 import Carousel from '@/components/carousselSection/Carousel';
 import HeroInfo1 from './heroInfo1';
 import HeroInfo2 from './heroInfo2';
 // import BackgroundTechnologies from './BackgroundTechnologies';
-const benefits: string[] = [
-  'Critical thinking applied to real-world challenges',
-  'Experience with scientific research and experimentation',
-  'Translating data into actionable insights',
-];
+// const benefits: string[] = [
+//   'Critical thinking applied to real-world challenges',
+//   'Experience with scientific research and experimentation',
+//   'Translating data into actionable insights',
+// ];
 
 export default function TwoColumnsWithImage() {
   return (
