375e9b172778e6bbe0b1f05abdf0320b13a78c14
Olivierkepler <olivierkfrancois1@gmail.com>
2025-04-22 19:28:39 -0400

    adding newletter section

--- components/carousselSection/heroInfo.tsx
+++ components/carousselSection/heroInfo.tsx
@@ -88,7 +88,7 @@ export default function HeroInfo() {
           variants={fadeUp}
           className="mb-6 text-4xl font-extrabold leading-tight text-white md:text-5xl"
         >
-          Hi, I'm Olivier Kepler François
+          Hi, I m Olivier Kepler François
         </motion.h1>
 
         <motion.p
