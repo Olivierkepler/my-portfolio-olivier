073a26a8d905d72c5138329f52e2341e7cac0bc2
Olivierkepler <olivierkfrancois1@gmail.com>
2025-04-24 20:45:21 -0400

    update caroussel

--- components/projects/project1.tsx
+++ components/projects/project1.tsx
@@ -1,5 +1,10 @@
 export default function Project1() {
   return <div className="sticky top-0 z-40 bg-black h-screen flex items-center justify-center">
-  <h1 className="text-8xl">Project 1</h1>
+ 
+
+     <div className="flex gap-20 text-white text-8xl font-bold items-center justify-center">
+ <span>Project</span><img src="/images/womanImage.jpg" alt="arrow" className="w-200 h-200" /> 
+     
+     <span>Design</span></div>
 </div>;
 }
