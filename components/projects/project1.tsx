b9c74dfa865166a473a9c12414ab56780913a3c0
Olivierkepler <olivierkfrancois1@gmail.com>
2025-04-24 21:19:43 -0400

    update caroussel

--- components/projects/project1.tsx
+++ components/projects/project1.tsx
@@ -1,18 +1,33 @@
 export default function Project1() {
-  return <div className="sticky top-0 z-40 bg-black h-screen flex items-center justify-center">
- 
-
- <div className="flex gap-4 text-white text-8xl font-bold items-center justify-center">
-  <span>Project</span>
+    return (
+      <div className="sticky top-0 z-40 bg-black h-screen flex items-center justify-center">
+        <div className="flex flex-col justify-center items-center mx-auto">
+          <div className="flex items-center justify-center text-white text-8xl font-bold relative">
+            <span className="mr-6">Project</span>
   
-  <div className="flex gap-2">
-    <img src="/images/womanImage.jpg" alt="image1" className="w-200 h-200 object-cover " />
-    <img src="/images/womanImage.jpg" alt="image2" className="w-200 h-200 object-cover -translate-x-200" />
-    <img src="/images/womanImage.jpg" alt="image3" className="w-200 h-200 object-cover -translate-x-400" />
-  </div>
-
-    <span className="text-white text-8xl font-bold -translate-x-290">Design</span>
-</div>
-
-</div>;
-}
+            {/* Overlayed Images */}
+            <div className="relative w-48 h-48">
+              <img
+                src="/images/womanImage.jpg"
+                alt="image1"
+                className="absolute top-0 left-0 w-48 h-48 object-cover z-30"
+              />
+              <img
+                src="/images/womanImage.jpg"
+                alt="image2"
+                className="absolute top-0 left-0 w-48 h-48 object-cover z-20"
+              />
+              <img
+                src="/images/womanImage.jpg"
+                alt="image3"
+                className="absolute top-0 left-0 w-48 h-48 object-cover z-10"
+              />
+            </div>
+  
+            <span className="ml-6">Design</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
